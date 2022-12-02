da3fe35b9b99cd143d078bc33ea3e672114fb7a8
IBaronap <isabella.baronap@gmail.com>
2022-12-02 15:05:53 -0500

    Proyecto

--- ProyectoFinal-Isabella Barona/app/components/Home/Feed/components/index.ts
+++
@@ -1,9 +0,0 @@
-import "./Counter/Counter.js"
-import "./AddPost/Send.js"
-
-import "./Profile/New.js"
-import "./Profile/Profile.js"
-
-import "./NavBar/Nav.js"
-import "./stories/Stories.js"
-import "./Sidebar/Sidebar.js"
--- ProyectoFinal-Isabella Barona/app/components/index.ts
+++
@@ -1,6 +0,0 @@
-export {Form} from "./Form/Form.js"
-export {AddPost} from "./Form/AddPost.js"
-
-export { Home } from "./Home/Home.js"
-export { Login } from "./Login/Login.js"
-export { Register } from "./Register/Register.js"
